fb7eb58d80b533e025e35cf3396acd98a44aa6bc
youngseok.jang <ys123.jang@gmail.com>
2024-06-15 17:28:51 +0900

    add udp multicast

---
+++ UDP-multicasting/udp-multicast.rs
@@ -0,0 +1,35 @@
+use std::{
+    env,
+    net::{Ipv4Addr, UdpSocket},
+    str,
+};
+
+fn main() {
+    let mcast_group: Ipv4Addr = "239.0.0.1".parse().unwrap();
+    let port = 6_000_u16;
+    let any = "0.0.0.0".parse().unwrap();
+    let mut buffer = [0u8; 1600];
+
+    if env::args().count() > 1 {
+        // client case
+        let socket = UdpSocket::bind((any, port)).expect("Could not bind client socket");
+        socket
+            .join_multicast_v4(&mcast_group, &any)
+            .expect("Could not join multicast group");
+
+        socket
+            .recv_from(&mut buffer)
+            .expect("Failed to write to server");
+
+        print!(
+            "{}",
+            str::from_utf8(&buffer).expect("Could not write buffer as string")
+        );
+    } else {
+        // server case
+        let socket = UdpSocket::bind((any, 0)).expect("Could not bind socket");
+        socket
+            .send_to("hello world!".as_bytes(), &(mcast_group, port))
+            .expect("Failed to write data");
+    }
+}
